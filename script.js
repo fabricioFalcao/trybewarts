80dea92ac1bd471f5160c4a115995c66e3527e6e
fabricioFalcao <fabriciohfd@gmail.com>
2023-05-30 22:20:00 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ const textArea = document.querySelector('#textarea');
 textArea.addEventListener('keyup', () => {
   counter.innerText = 500 - textArea.value.length;
 });
+
+const disciplines = () => {
+  const subjects = document.querySelectorAll('.subject');
+  const chosenDsiciplines = [];
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (subjects[index].checked) {
+      chosenDsiciplines.push(subjects[index].value);
+    }
+  }
+  return chosenDsiciplines.join(', ');
+};
+
+button2.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  const name = document.getElementById('input-name').value;
+  const surname = document.getElementById('input-lastname').value;
+  const inputEmail = document.getElementById('input-email').value;
+  const house = document.getElementById('house').value;
+  const family = document.querySelector('input[name=family]:checked').value;
+  const grade = document.querySelector('input[name=rate]:checked').value;
+  const opinion = document.getElementById('textarea').value;
+  document.getElementById('evaluation-form').hidden = true;
+  document.getElementById('form-data').hidden = false;
+  document.getElementById('p1').innerText = `Nome: ${name} ${surname}`;
+  document.getElementById('p2').innerText = `Email: ${inputEmail}`;
+  document.getElementById('p3').innerText = `Casa: ${house}`;
+  document.getElementById('p4').innerText = `Família: ${family}`;
+  document.getElementById('p5').innerText = `Matérias: ${disciplines()}`;
+  document.getElementById('p6').innerText = `Avaliação: ${grade}`;
+  document.getElementById('p7').innerText = `Observações: ${opinion}`;
+});
